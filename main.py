9a75fd3fd9364550b0a55126ea1f01fb3d693055
AlexandreGoettel <alexandre.goettel@rwth-aachen.de>
2022-05-16 22:02:33 +0200

    Added propagate function."

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ class Grid:
 
     def __init__(self, n=9):
         """Init an empty grid."""
+        assert not n % 3
+
         dt = np.dtype([('val', int), ('options', int, (n,))])
         a = np.zeros(shape=(n, n), dtype=dt)
         self.grid = a.view(np.recarray)
@@ -49,15 +51,60 @@ class Grid:
     def setVal(self, val, i, j):
         """Set a value at a particular grid coord."""
         case = self.grid[i, j]
-        case["val"], case["options"] = val, np.zeros(self.n)
+        case["val"] = val
+        case["options"] = np.zeros(self.n) if val else np.ones(self.n)
+    
+    def propagate(self, i, j, val):
+        """Insert a value in a case and update neighbouring options."""
+        # Update case
+        self.grid[i, j]["val"] = val
+        self.grid[i, j]["options"] = np.zeros(self.n)
+        
+        # Get options of neighbouring cases
+        m = int(self.n / 3)
+        (i // m)*m
+        mSqr = self.grid[(i // m)*m:(i // m + 1)*m,
+                         (j // m)*m:(j // m + 1)*m]["options"]
+        mRow = self.grid[i, :]["options"]
+        mCol = self.grid[:, j]["options"]
+        
+        # Update those cases
+        def doUpdate(_list):
+            _list[..., val-1] = 0
+        list(map(doUpdate, [mRow, mCol, mSqr]))
+        
     
     def solve(self):
-        # TODO
-
+        """Solve the sudoku grid."""
+        # 1. Initialise
+        # Collapse wave functions around known values
+        for i, row in enumerate(self.grid):
+            for j, case in enumerate(row):
+                val = case.val
+                if val:
+                    self.propagate(i, j, val)
+        
+        n_options = np.sum(self.grid["options"], axis=-1)
+        print(np.where(n_options == 1))
+        self.propagate(4, 8, 3)
+        n_options = np.sum(self.grid["options"], axis=-1)
+        print(np.array(n_options == 1, dtype=int))
+        print(np.where(n_options == 1))
+        print(self.grid[4, 0])
+        self.propagate(4, 0, 6)
+        n_options = np.sum(self.grid["options"], axis=-1)
+        print(np.array(n_options == 1, dtype=int))
+        print(np.where(n_options == 1))
+        # 2. Create list of "known" variables
+        
+        # 3. Continue until no viable options are left
+        # 4. ???
+        # 5. Profit
 
 def main():
     g = Grid(9)
     g.loadStd()
+    print(g.grid["val"], "\n----------------------------")
     g.solve()
 
 

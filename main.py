5a47a060daa4f3e91a8a1c1a485a33312e3fd94f
AlexandreGoettel <alexandre.goettel@rwth-aachen.de>
2022-05-17 19:48:51 +0200

    Can now solve all but hardest sudoku.

--- main.py
+++ main.py
@@ -15,6 +15,24 @@ class Grid:
         self.n = n
         self.resetOptions()
     
+    def __str__(self):
+        """String representation for print."""
+        _str = ""
+        m = int(self.n / 3)
+        for i in range(self.n):
+            if not i % m:
+                _str += "#############\n"
+            for j in range(self.n):
+                if not j % m:
+                    _str += "#"
+                _str += "{:d}".format(self.grid[i, j]["val"])
+            _str += "#\n"
+        return _str + "#############"
+    
+    def copy(self, _grid):
+        self.grid = np.array(_grid.grid)
+        self.n = int(_grid.n)
+    
     def resetOptions(self):
         """"Set options to one everywhere."""
         for i in range(self.n):
@@ -44,6 +62,15 @@ class Grid:
                    0, 3, 0, 0, 8, 0, 0, 0, 0,
                    1, 0, 2, 7, 0, 0, 0, 0, 0,
                    7, 0, 9, 0, 5, 0, 0, 0, 0]
+        numbers = [0, 0, 5, 4, 0, 0, 0, 0, 3,
+                   0, 0, 0, 2, 1, 5, 0, 0, 0,
+                   0, 0, 2, 0, 0, 0, 0, 0, 0,
+                   0, 0, 6, 3, 4, 0, 0, 1, 0,
+                   0, 3, 4, 0, 0, 0, 2, 8, 0,
+                   0, 2, 0, 0, 5, 7, 4, 0, 0,
+                   0, 0, 0, 0, 0, 0, 9, 0, 0,
+                   0, 0, 0, 1, 3, 8, 0, 0, 0,
+                   7, 0, 0, 0, 0, 4, 3, 0, 0]
         for m, val in enumerate(numbers):
             i, j = m // self.n, m % self.n
             self.setVal(val, i, j)
@@ -62,7 +89,6 @@ class Grid:
         
         # Get options of neighbouring cases
         m = int(self.n / 3)
-        (i // m)*m
         mSqr = self.grid[(i // m)*m:(i // m + 1)*m,
                          (j // m)*m:(j // m + 1)*m]["options"]
         mRow = self.grid[i, :]["options"]
@@ -74,38 +100,129 @@ class Grid:
         list(map(doUpdate, [mRow, mCol, mSqr]))
         
     
-    def solve(self):
-        """Solve the sudoku grid."""
-        # 1. Initialise
-        # Collapse wave functions around known values
+    def getChanges(self, n_target=1):
+        """Get coordinates and val of proposed changes to the grid."""
+        n_options = np.sum(self.grid["options"], axis=-1)
+        x, y = np.where(n_options == n_target)
+        _, z = np.where(self.grid[x, y]["options"])
+        if n_target > 1:
+            z = z.reshape((len(x), n_target))
+        return x, y, z + 1
+
+    def solve(self, verbose=False):
+        """Solve the sudoku grid until simple wave collapse limit."""
+        # 1. Initialise: collapse wave functions around known values
+        # TODO: don't repeat this step in later solve() calls
         for i, row in enumerate(self.grid):
             for j, case in enumerate(row):
                 val = case.val
                 if val:
                     self.propagate(i, j, val)
-        
-        n_options = np.sum(self.grid["options"], axis=-1)
-        print(np.where(n_options == 1))
-        self.propagate(4, 8, 3)
-        n_options = np.sum(self.grid["options"], axis=-1)
-        print(np.array(n_options == 1, dtype=int))
-        print(np.where(n_options == 1))
-        print(self.grid[4, 0])
-        self.propagate(4, 0, 6)
-        n_options = np.sum(self.grid["options"], axis=-1)
-        print(np.array(n_options == 1, dtype=int))
-        print(np.where(n_options == 1))
-        # 2. Create list of "known" variables
-        
-        # 3. Continue until no viable options are left
-        # 4. ???
-        # 5. Profit
 
-def main():
-    g = Grid(9)
+        # 2. Create list of "known" cases with option length one      
+        x, y, z = self.getChanges()
+        
+        # 3. Propagate and repeat until no viable options are left
+        while len(x):
+            # Only first because of possible updates
+            i, j, val = list(zip(x, y, z))[0]
+            if verbose:
+                print("Inserting {:d} in [{:d},{:d}]..".format(val, i, j))
+            self.propagate(i, j, val)
+            x, y, z = self.getChanges()
+    
+    def isSolved(self):
+        """Check if grid is solved."""
+        return not 0 in self.grid["val"]
+    
+    def isValid(self):
+        """Check if the grid makes sense."""
+        def checkRow(row):
+            _, counts = np.unique(row[row != 0], return_counts=True)
+            return not np.sum(np.array(counts != 1, dtype=int))
+
+        for i, row in enumerate(self.grid["val"]):
+            # Check row
+            if not checkRow(row):
+                return False
+            
+            # Check column
+            col = self.grid[:, i]["val"]
+            if not checkRow(col):
+                return False
+            
+            # Check square
+            j, k = [[0, 0], [0, 3], [0, 6],
+                    [3, 0], [3, 3], [3, 6],
+                    [6, 0], [6, 3], [6, 6]][i]
+            m = int(self.n / 3)
+            sqr = self.grid[(j // m)*m:(j // m + 1)*m,
+                            (k // m)*m:(k // m + 1)*m]["val"].flatten()
+            if not checkRow(sqr):
+                return False
+            
+            # Check cases
+            for j, val in enumerate(row):
+                if not val + np.sum(self.grid[i, j]["options"]):
+                    return False
+        return True
+
+
+
+def branchOut(grid, i, j, values):
+    """Branch out between the values and return newly-solved grids."""
+    _ret = []
+    for val in values:
+        _grid = Grid(grid.n)
+        _grid.copy(grid)
+        _grid.propagate(i, j, val)
+        _grid.solve()
+        _ret += [_grid]
+    return _ret
+
+
+def createUniverses(grid):
+    # Branch out parallel universes
+    x, y, z = grid.getChanges(2)
+    
+    for i, j, values in zip(x, y, z):
+        gridOne, gridTwo = branchOut(grid, i, j, values)
+        
+        # Pick best option, insert, repeat
+        g1_solved, g1_valid = gridOne.isSolved(), gridOne.isValid()
+        g2_solved, g2_valid = gridTwo.isSolved(), gridTwo.isValid()
+    
+        if g1_solved and g1_valid:
+            return gridOne
+        elif g2_solved and g2_valid:
+            return gridTwo
+        elif g1_valid and not g2_valid:
+            return gridOne
+        elif g2_valid and not g1_valid:
+            return gridTwo
+        else:
+            continue
+    
+    print("Best solution:")
+    print(grid)
+    raise Exception("Sudoku could not be solved!")
+
+
+def main(nGrid=9):
+    g = Grid(nGrid)
     g.loadStd()
-    print(g.grid["val"], "\n----------------------------")
+    print("Initial grid:")
+    print(g, "\n----------------------------")
+
+    # Try to solve directly
     g.solve()
+    if g.isSolved(): return
+    
+    # Solve with branching
+    while not g.isSolved():
+        newGrid = createUniverses(g)
+        g.copy(newGrid)
+    print("Solution:\n", g)
 
 
 if __name__ == '__main__':

db4dcb824674c8e1b15fe018bf9b8a3c408eaefc
AlexandreGoettel <alexandre.goettel@rwth-aachen.de>
2022-07-31 21:31:53 +0100

    Prepare for training steps.

--- network_v3.py
+++ network_v3.py
@@ -1,6 +1,5 @@
 """Train MNIST based on https://nextjournal.com/gkoehler/pytorch-mnist."""
 # Standard imports
-import random
 import numpy as np
 from matplotlib import pyplot as plt
 # Torch
@@ -9,17 +8,21 @@ import torchvision
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from torchinfo import summary
 # Project imports
-from  getfontdata import fontData
+from  getfontdata import FontData
 
 
 class ImageClassifier(nn.Module):
     
-    def __init__(self, kernel_size=5):
+    def __init__(self, kernel_size=5, dropout=.3):
         super(ImageClassifier, self).__init__()
+        # Hyper-params
+        self.dropout = dropout
+        # Network
         self.conv1 = nn.Conv2d(1, 10, kernel_size=kernel_size)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=kernel_size)
-        self.conv2_drop = nn.Dropout2d()
+        self.conv2_drop = nn.Dropout2d(p=dropout)
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
     
@@ -28,39 +31,50 @@ class ImageClassifier(nn.Module):
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
+        x = F.dropout(x, training=self.training, p=self.dropout)
         x = self.fc2(x)
         return F.log_softmax(x)
 
 
-
-
-
-def train(train_loader, val_loader, test_loader):
+def train(train_loader, val_loader, test_loader,
+          dropout=.3, lr=.001, momentum=.9, lr_factor=.1, epochs=100):
     """Train the "ImageClassifier" using the passed datasets."""
-    
+    model = ImageClassifier(dropout=dropout)
+    print(summary(model, (64, 1, 28, 28)))
 
 
 def main():
     # Set hyperparameters
     batch_size_train = 64
-    
-    mnist_train_data = torchvision.datasets.MNIST(
-        "mnist/", train=True, download=True,
-        transform=torchvision.transforms.ToTensor()).data
-    
-    fonts_train_data = fontData(mnist_train_data.numpy())
-    
-    fonts_train_loader = torch.utils.data.DataLoader(
-        fontData(mnist_train_data.numpy()), batch_size=10, shuffle=True)
+    validation_size = 5000
 
     # Get training, validation, and test data from mnist
+    mnist_train_data = torchvision.datasets.MNIST(
+        "mnist/", train=True, download=True,
+        transform=torchvision.transforms.ToTensor())
+    
+    mnist_train_data, mnist_val_data = torch.utils.data.random_split(
+        mnist_train_data, [len(mnist_train_data) - validation_size,
+                           validation_size])
+
     mnist_train_loader = torch.utils.data.DataLoader(
+        mnist_train_data, batch_size=batch_size_train, shuffle=True)
+    
+    mnist_val_loader = torch.utils.data.DataLoader(
+        mnist_val_data, batch_size=batch_size_train, shuffle=True)
+
+    mnist_test_loader = torch.utils.data.DataLoader(
         torchvision.datasets.MNIST(
-            "mnist/", train=True, download=True,
+            "mnist/", train=False, download=True,
             transform=torchvision.transforms.ToTensor()),
         batch_size=batch_size_train, shuffle=True)
     
+    train(mnist_train_loader, mnist_val_loader, mnist_test_loader)
+    
+    # fonts_train_loader = torch.utils.data.DataLoader(
+    #     FontData(mnist_train_data.dataset.data.numpy()),
+    #     batch_size=10, shuffle=True)
+
     # Combine with font-generated data
     # TODO: NORMALISE PADDING?
     # Make sure there is enough data augmentation
